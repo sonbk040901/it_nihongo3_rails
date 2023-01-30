8240967010a005130c8df26d183b8a922db60ee9
sonbk040901 <leducon007@gmail.com>
2023-01-30 05:25:21 +0000

    add_name_age_to_user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/show'
   root 'static_pages#home'
 #   get 'static_pages/home'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   devise_for :models
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
