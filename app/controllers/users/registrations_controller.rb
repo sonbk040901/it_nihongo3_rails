1f4399c26eb51c0bc125ffdf020d1076c9f0f5eb
sonbk040901 <leducon007@gmail.com>
2023-01-30 05:33:51 +0000

    add_image_to_user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -45,7 +45,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 #      devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
 #   end
     def configure_sign_up_params
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :avatar])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :avatar])
     end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
